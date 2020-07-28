6c60b5842b41659c6131d6c988282036a4c2b8d1
jcmcarth06 <jcmcarth06@gmail.com>
2020-07-27 20:24:59 -0600

    change content key in comments table

--- db/migrate/20200727230832_create_comments.rb
+++ db/migrate/20200727230832_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :body
+      t.text :content
 
       t.references :book, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
