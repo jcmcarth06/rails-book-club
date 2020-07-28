8c217cb9ed5d6e4f01b36f3905063703086eb870
jcmcarth06 <jcmcarth06@gmail.com>
2020-07-28 14:57:37 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
-  get '/signin' => 'sessions#new'
-  post '/signin' => 'sessions#create'
-  get '/signout' => 'sessions#destroy'
+
+  get '/signup' to: 'users#new'
+  post '/signup' to: 'users#create'
+  get '/users/:id' to: 'users#show' as: 'user'
+
+  get '/signin' to: 'sessions#new'
+  post '/signin' to: 'sessions#create'
+  delete '/signout' to: 'sessions#destroy'
+
 
   resources :users, only: [:new, :create, :show]
   resources :comments
